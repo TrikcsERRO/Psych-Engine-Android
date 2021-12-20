038e48c09c67c5c277cd210a074180b2f2e70c69
TrikcsERRO <96119468+TrikcsERRO@users.noreply.github.com>
2021-12-20 11:18:50 -0500

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -929,7 +929,7 @@ class PlayState extends MusicBeatState
 		add(iconP2);
 		reloadHealthBarColors();
 
-        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Tricky The [%ERROR%]"), 24);
+        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Tiky"), 24);
         creditTxt.scrollFactor.set();
         creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         creditTxt.borderColor = FlxColor.BLACK;
